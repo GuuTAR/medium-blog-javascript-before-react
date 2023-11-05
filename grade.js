41441290f2985b4c4b64846675ea4b595ca67433
GuuTAR <guitar20122012@gmail.com>
2023-11-05 15:39:38 +0700

    Simple grade program

---
+++ grade.js
@@ -0,0 +1,17 @@
+const score = 85
+
+if (score >= 80) {
+    console.log('A')
+}
+else if (score < 80 && score >= 70) {
+    console.log('B')
+}
+else if (score < 70 && score >= 60) {
+    console.log('C')
+}
+else if (score < 60 && score >= 50) {
+    console.log('D')
+}
+else {
+    console.log('F')
+}
